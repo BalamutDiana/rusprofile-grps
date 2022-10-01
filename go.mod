62a51972027f9e3b32116a2cf96805b168f92985
BalamutDiana <67107383+BalamutDiana@users.noreply.github.com>
2022-10-01 15:24:53 +0300

    add modules

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/BalamutDiana/rusprofile-grps
+
+go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20220930213112-107f3e3c3b0b // indirect
+)
--- internal/service/company.go
+++ internal/service/company.go
@@ -1,1 +1,80 @@
 package company
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type ContentsData struct {
+	UL []Info `json:"ul"`
+}
+
+type Info struct {
+	Name    string `json:"name"`
+	Link    string `json:"link"`
+	INN     string `json:"inn"`
+	CeoName string `json:"ceo_name"`
+}
+
+func GetMainInfo(inn string) {
+	rand.Seed(time.Now().Unix())
+
+	rowUrl := "https://www.rusprofile.ru/ajax.php?query=%s&action=search&cacheKey=%.12f"
+	url := fmt.Sprintf(rowUrl, inn, rand.Float64())
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var respOne ContentsData
+
+	err = json.Unmarshal(body, &respOne)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(respOne.UL[0].Name)
+	fmt.Println(respOne.UL[0].INN)
+	fmt.Println(GetKPP(inn))
+	fmt.Println(respOne.UL[0].CeoName)
+}
+
+func GetKPP(inn string) string {
+	rowUrl := "https://www.rusprofile.ru/search?query=%s&type=ul"
+	url := fmt.Sprintf(rowUrl, inn)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var spans []string
+	doc.Find(".dashboard-page .company-col dd").Each(func(i int, s *goquery.Selection) {
+		item := s.Find("span").Text()
+		spans = append(spans, item)
+	})
+	return spans[3]
+}
